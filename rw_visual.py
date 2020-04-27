77c71af5aec6c83509092145a6bfa4296aaeac08
dexmoss <moxmo73@gmail.com>
2020-04-27 15:27:17 +0200

    Using figure() to show plot in bigger resolution, increase number of points from 5,000 to 50,000, change the size of first and the last point to 25

--- rw_visual.py
+++ rw_visual.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 from randomwalk import RandomWalkClass
 
 while True:
-    rw = RandomWalkClass()
+    rw = RandomWalkClass(50000)
     rw.fill_walk()
 
+    # Create figure from internal function
+    plt.figure(dpi=128, figsize=(10, 6))
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolor='none', s=15)
+                edgecolor='none', s=1)
     # Change the color of first (green) and last (red) points
-    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.scatter(0, 0, c='green', edgecolors='none', s=25)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=25)
     # Removing the axis from figure
     plt.axes().get_xaxis().set_visible(False)
     plt.axes().get_yaxis().set_visible(False)
