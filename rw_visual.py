b7081f16de5a55ae6427bf278a3c50f0d23d5646
dexmoss <moxmo73@gmail.com>
2020-04-27 14:57:36 +0200

    Add feature: more RandomWalk calls

--- rw_visual.py
+++ rw_visual.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 
 from randomwalk import RandomWalkClass
 
-rw = RandomWalkClass()
-rw.fill_walk()
+while True:
+    rw = RandomWalkClass()
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
+    if keep_running == 'n':
+        break
