516f720384573498890785f1048192c0858528da
dexmoss <moxmo73@gmail.com>
2020-04-28 15:43:59 +0200

    Add second die and results of 2 dies rolls

--- PYGAL_FILES/random_number_visual.py
+++ PYGAL_FILES/random_number_visual.py
@@ -1,18 +1,20 @@
 import pygal
 from random_number import Die
 
-# creation the die with 6 walls
-die = Die()
+# creation the two dies with 6 walls
+die_1 = Die()
+die_2 = Die()
 
 # make some number of rolls and pull them on the list
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # results analysis
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(1, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -20,10 +22,10 @@ for value in range(1, die.num_sides+1):
 hist = pygal.Bar()
 hist.force_uri_protocol = 'http'
 
-hist.title = "Results of 1 die rolls of 1000 times"
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.title = "Results of 2 dies rolls of 1000 times"
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = "Result"
 hist.y_title = "Frequencies of result occurrence"
 
-hist.add('D6', frequencies)
-hist.render_to_file('die_visual.svg')
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('two_dies_visual.svg')
