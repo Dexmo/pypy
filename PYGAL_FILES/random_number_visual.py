8104bdaf9f036003f969030ed7063b891ffa485d
dexmoss <moxmo73@gmail.com>
2020-04-28 15:04:37 +0200

    1) Change the number of rolls from 100 to 1000; 2) add freguencies and code which counts the number of each number occurence

--- PYGAL_FILES/random_number_visual.py
+++ PYGAL_FILES/random_number_visual.py
@@ -5,8 +5,14 @@ die = Die()
 
 # make some number of rolls and pull them on the list
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# results analysis
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
