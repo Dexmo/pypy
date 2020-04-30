a8c0af00f6af109c7c7cb3f4cef5e8f89e13b2a4
dexmoss <moxmo73@gmail.com>
2020-04-30 16:23:49 +0200

    My child play with tuple and list

---
+++ playground.py
@@ -0,0 +1,9 @@
+values = (1,2,3,4,5) #tuple - immutable, orderly data structure
+squares = [value**2 for value in range(1,11)] #list comprehension
+
+''' 3 short subprogram for quick work '''
+print(max(values))
+print(min(values))
+print(sum(values))
+
+print(squares)
