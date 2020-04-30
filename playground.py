407a9327024b402750efd362e2441034fb45a571
dexmoss <moxmo73@gmail.com>
2020-04-30 17:24:02 +0200

    Add some operations as creating list with odd numbers and cube values.

--- playground.py
+++ playground.py
@@ -7,3 +7,21 @@ print(min(values))
 print(sum(values))
 
 print(squares)
+'''---------------------------------------'''
+for number in range(1, 21):
+    print(number)
+
+# creating list with 1000 values
+thousand_list = []
+for number in range(1,1001):
+    thousand_list.append(number)
+
+# creating list with odd numbers
+odd_number = []
+for number in range(1,20,2):
+    odd_number.append(number)
+print(odd_number)
+
+# creating the list with values to cube using list comprehension
+cube_values = [value**3 for value in range(1,11)]
+print(cube_values)
