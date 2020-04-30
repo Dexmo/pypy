441d737424338370f2bc895fde992ec0a85a1bad
dexmoss <moxmo73@gmail.com>
2020-04-30 15:57:14 +0200

    File with sorting examples - sort(), sorted() and reverse() methods.

---
+++ sorting_time.py
@@ -0,0 +1,11 @@
+places = ['Praga', 'Berlin', 'Moskwa', 'Rzym', 'Oslo']
+
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
