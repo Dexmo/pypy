817ab12be9fdff6028f042d7f7d5a8cac88fa951
dexmoss <moxmo73@gmail.com>
2020-05-01 11:57:59 +0200

    Add new keys and values to dictionary.

--- dictonaries.py
+++ dictonaries.py
@@ -1,4 +1,14 @@
+''' Short program using dictionaries'''
+# New dictionary
 alien_0 = {'color':'green', 'points':5}
 
+# Printing values of two elements
 print(alien_0['color'])
 print(alien_0['points'])
+
+# Adding new keys and values - do not forget about '' on keys!
+alien_0['position_x'] = 0
+alien_0['position_y'] = 5
+
+# Printing all dictionary
+print(alien_0)
