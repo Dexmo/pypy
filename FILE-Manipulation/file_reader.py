77bf36262ed7f26d2377128c569acafd89b62504
dexmoss <moxmo73@gmail.com>
2020-05-04 14:30:42 +0200

    Modification - program print the file line to line without empty lines.

--- FILE-Manipulation/file_reader.py
+++ FILE-Manipulation/file_reader.py
@@ -1,3 +1,7 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+# Print text line to line program
+
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
