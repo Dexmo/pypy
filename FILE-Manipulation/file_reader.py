5478608ec596308ed85973ad3c1d09bb5cf07643
dexmoss <moxmo73@gmail.com>
2020-05-04 14:36:52 +0200

    We remove all white marks and count the number of digits.

--- FILE-Manipulation/file_reader.py
+++ FILE-Manipulation/file_reader.py
@@ -5,5 +5,11 @@ filename = 'pi_digits.txt'
 with open(filename) as file_object:
     lines = file_object.readlines()
 
+pi_string = ''
+
+# We using .strip() instead of .rstrip() to remove all white marks
 for line in lines:
-    print(line.rstrip())
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
