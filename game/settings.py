82ce8ccf70e7a3bcd33e925801371d1bf786a4bc
dexmoss <moxmo73@gmail.com>
2020-05-06 14:52:37 +0200

    Creating new class with settings.

---
+++ game/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """ Class for all settings """
+
+    def __init__(self):
+        #Settings of screen
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 0)
