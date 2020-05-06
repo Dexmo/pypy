1118f8009eb6e6005f517d05ed889740b894abee
dexmoss <moxmo73@gmail.com>
2020-05-06 16:00:40 +0200

    Adding ship to the game.

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -1,30 +1,35 @@
 import sys
-
 import pygame
-
 from settings import Settings
+from ship import Ship
+
 
 def run_game():
-    #Game initial - create window
-    pygame.init() # Create the view
+    # Game initial - create window
+    pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
 
-    #Main loop
+    # Creating the ship
+    ship = Ship(screen)
+
+    # Main loop
     while True:
 
-        #Waiting for event
+        # Waiting for event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        #Refreshing the screen in each loop's iteration
+        # Refreshing the screen in each loop's iteration
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
-        #Show the last modified view
+        # Show the last modified view
         pygame.display.flip()
+
 
 run_game()
